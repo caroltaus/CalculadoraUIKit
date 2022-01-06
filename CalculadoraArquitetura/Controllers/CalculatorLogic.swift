45da59d0fe9061dffbe3e947f64c49f36aff9067
caroltaus <carolinetaus@hotmail.com>
2022-01-06 18:09:53 -0300

    grandes dificuldades

---
+++ CalculadoraArquitetura/Controllers/CalculatorLogic.swift
@@ -0,0 +1,26 @@
+//
+//  CalculatorLogic.swift
+//  CalculadoraArquitetura
+//
+//  Created by Caroline Taus on 06/01/22.
+//
+
+import Foundation
+
+class CalculatorLogic {
+    func sum(_ a: Int, _ b: Int) -> Int {
+        return a+b
+    }
+    
+    func sub(_ a: Int, _ b: Int) -> Int {
+        return a-b
+    }
+    
+    func mult(_ a: Int, _ b: Int) -> Int {
+        return a*b
+    }
+    
+    func div(_ a: Int, _ b: Int) -> Int {
+        return a/b
+    }
+}
---
+++ CalculadoraArquitetura/View/CalculatorView.swift
@@ -0,0 +1,15 @@
+//
+//  CalculatorView.swift
+//  CalculadoraArquitetura
+//
+//  Created by Caroline Taus on 06/01/22.
+//
+
+import Foundation
+import UIKit
+
+class CalculatorView: UIView {
+    
+    
+    
+}
